87aba0101b7c5d8c284baa6c10e1cdae900fe34d
MisaDevs <misavazquezsifuentes@gmail.com>
2026-03-22 16:53:47 -0600

    Interface update

--- proyecto-logica-estructuras-discretas/main.py
+++ proyecto-logica-estructuras-discretas/main.py
@@ -1,27 +1,21 @@
 from flask import Flask, render_template, request
 from logic import validar_argumento
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static')
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     resultado = None
     if request.method == 'POST':
-        
         premisas_raw = request.form.get('premisas', '')
         conclusion = request.form.get('conclusion', '')
-        
-        
         lista_premisas = [p.strip() for p in premisas_raw.split('\n') if p.strip()]
-        
-       
+
         valido, tabla, variables, criticos = validar_argumento(lista_premisas, conclusion)
-        
-        
+
         if valido == "Error de sintaxis":
             resultado = {"error": "Hubo un error en la sintaxis de tus fórmulas. Revisa los operadores."}
         else:
-            
             resultado = {
                 'valido': valido,
                 'tabla': tabla,
@@ -30,7 +24,6 @@ def index():
                 'conclusion_nombre': conclusion
             }
 
-    
     return render_template('index.html', resultado=resultado, all=all)
 
 if __name__ == '__main__':
